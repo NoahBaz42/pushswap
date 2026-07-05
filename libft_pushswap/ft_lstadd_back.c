16af28cea9558e2341419655d35743a905138398
Charles Eyler <bpassos-@student.42lisboa.com>
2026-07-05 22:49:29 +0100

    ft_lstaddback main commented

--- libft_pushswap/ft_lstadd_back.c
+++ libft_pushswap/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bpassos- <bpassos-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 21:56:10 by bpassos-          #+#    #+#             */
-/*   Updated: 2026/07/05 22:23:29 by bpassos-         ###   ########.fr       */
+/*   Updated: 2026/07/05 22:48:53 by bpassos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,54 +29,54 @@ static void	del_it(void *content)
 	free(content);
 }
 
-static void ft_print_lst(t_node *first)
-{
-	t_node	*temp;
-	int		num;
-	int		i;
+// static void ft_print_lst(t_node *first)
+// {
+// 	t_node	*temp;
+// 	int		num;
+// 	int		i;
 
-	i = 1;
-	temp = first;
-	while (temp)
-	{
-		num = *(int *)temp->content;
-		printf("%d: %d\n", i, num);
-		i++;
-		temp = temp->next;
-		if (i > 11)
-			break ;
-	}
-}
+// 	i = 1;
+// 	temp = first;
+// 	while (temp)
+// 	{
+// 		num = *(int *)temp->content;
+// 		printf("%d: %d\n", i, num);
+// 		i++;
+// 		temp = temp->next;
+// 		if (i > 11)
+// 			break ;
+// 	}
+// }
 
-int main (int argc, char **argv)
-{
-	int *x;
-	int	i;
-	t_node *node;
-	t_node *new_node;
-	t_node	*temp;
+// int main (int argc, char **argv)
+// {
+// 	int *x;
+// 	int	i;
+// 	t_node *node;
+// 	t_node *new_node;
+// 	t_node	*temp;
 
-	i = 1;
-	if (argc < 2)
-		return (printf("incorrect # of arguments\n"), 1);
-	x = malloc(sizeof(x));
-	*x = ft_atoi(argv[i++]);
-	node = ft_lstnew(x);
-	temp = node;
-	while (i < argc)
-	{
-		x = malloc(sizeof(x));
-		*x = ft_atoi(argv[i++]);
-		new_node = ft_lstnew(x);
-		ft_lstadd_back( &temp, new_node);
-		// printf("---\nnew node [%d] data: [%p] %d start @ [%p] %d\n",
-		// 	i - 1,
-		// 	temp->next, *(int *)temp->next->content,
-		// 	node, *(int *)node->content
-		// );
-		temp = temp->next;
-	}
-	ft_print_lst(temp);
-	ft_lstclear(&temp, del_it);
-	return (0);
-}
+// 	i = 1;
+// 	if (argc < 2)
+// 		return (printf("incorrect # of arguments\n"), 1);
+// 	x = malloc(sizeof(x));
+// 	*x = ft_atoi(argv[i++]);
+// 	node = ft_lstnew(x);
+// 	temp = node;
+// 	while (i < argc)
+// 	{
+// 		x = malloc(sizeof(x));
+// 		*x = ft_atoi(argv[i++]);
+// 		new_node = ft_lstnew(x);
+// 		ft_lstadd_back( &temp, new_node);
+// 		// printf("---\nnew node [%d] data: [%p] %d start @ [%p] %d\n",
+// 		// 	i - 1,
+// 		// 	temp->next, *(int *)temp->next->content,
+// 		// 	node, *(int *)node->content
+// 		// );
+// 		temp = temp->next;
+// 	}
+// 	ft_print_lst(temp);
+// 	ft_lstclear(&temp, del_it);
+// 	return (0);
+// }
