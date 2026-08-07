673e42dee1ffeb57ca1f118dc478908b6b8c4120
Charlie <bpassos-@student.42lisboa.com>
2026-08-07 04:35:21 +0100

    feat(algo): simple - min/max extraction sort

--- push_swap_algorithms/simple_alg.c
+++ push_swap_algorithms/simple_alg.c
@@ -3,31 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   simple_alg.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bpassos- <bpassos-@student.42lisboa.com>   +#+  +:+       +#+        */
+/*   By: charlie <charlie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 19:26:09 by bpassos-          #+#    #+#             */
-/*   Updated: 2026/07/16 11:22:37 by bpassos-         ###   ########.fr       */
+/*   Updated: 2026/08/06 21:21:51 by charlie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void    ft_selection_sort(t_node *a)
+void	ft_simple(t_node **a)
 {
-    t_node  *b;
-    t_node  *temp;
+	t_node *b;
+	t_node *min;
 
-    b = ft_lstnew(NULL);
-    while (a)
-    {
-        while(a)
-        {
-            temp->content = a->content;
-            if (temp->content < a->next->content)
-               temp->content = a->next->content;
-        a = a->next;
-        }
-        ft_pstack(temp->content, b->content);
-    }
-    ft_pushall_b(b, a);
+	b = NULL;
+	while (!ft_is_sorted(*a))
+	{
+		min = ft_find_min(*a);
+		if (ft_small_path_min(*a))
+			while ((*a)->content != min->content)
+				ft_rstack(a);
+		else
+			while((*a)->content != min->content)
+				ft_rrstack(a);
+		ft_pstack(a, &b);
+	}
+	ft_pushall_a(&b, a);
 }
+// -----------find_max----------//
+// int	main(int argc,char **argv)
+// {
+// 	t_node	*top;
+// 	int		i;
+
+// 	i = 1;
+// 	top = NULL;
+// 	if (argc < 2)
+// 		return (printf("incorrect # of arguments\n"), 1);
+// 	printf("Unsorted:\n");
+// 	while (i < argc)
+// 	{
+// 		ft_lstadd_back(&top, ft_lstnew(atoi( argv[i])));
+// 		i++;
+// 	}
+// 	ft_simple(&top);
+// }
