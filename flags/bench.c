dbee95a5836f7fa3baf8b5f31956f50ea9865305
Charlie <bpassos-@student.42lisboa.com>
2026-08-13 13:51:20 +0100

    Chore(bench): Flag_bench was split into bench and bench_utils

---
+++ flags/bench.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bench.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: charlie <charlie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/09 08:52:33 by username         ##+#    #+#             */
+/*   Updated: 2026/08/13 08:43:22 by charlie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pushswap.h"
+
+
+
+//----------[Prints the benchmark]--------------------//
+
+void	output_bench(t_op_count *op_count, t_flags *flags, float disorder)
+{
+	const char	*names[4] = {"Adaptive", "Simple", "Medium", "Complex"};
+	const char	*complex[4] = {"__", "O(n2)", "O(n√n)", "O(n log n)"};
+
+	ft_printf(2, "[bench] disorder: %d", (int)(disorder * 100));
+	ft_printf(2, ".%d", ((int)(disorder * 100) % 10));
+	ft_printf(2, "%d%%\n", (((int)(disorder * 1000) % 10)));
+	ft_printf(2, "[bench] strategy: %s / %s\n", names[flags->chosen],
+		complex[flags->difficulty]);
+	ft_printf(2, "[bench] total_ops: %d\n", op_count->total);
+	ft_printf(2, "[bench] sa: %d sb: %d ss: %d pa: %d pb: %d\n", op_count->sa,
+		op_count->sb, op_count->ss, op_count->pa, op_count->pb);
+	ft_printf(2, "[bench] ra: %d rb: %d rr: %d rra: %d rrb: %d rrr: %d\n",
+		op_count->ra, op_count->rb, op_count->rr, op_count->rra, op_count->rrb,
+		op_count->rrr);
+}
---
+++ flags/bench_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bench_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: charlie <charlie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/13 07:02:32 by charlie           #+#    #+#             */
+/*   Updated: 2026/08/13 08:43:00 by charlie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pushswap.h"
+//------[Checks for the flag (--bench) on the 1st and 2nd args]-----------//
+//------[gives error if there´s more then 1 bench flag]-------------------//
+//------------(needs an error dealing revision)---------------------------//
+
+void	flag_bench(t_flags *flags, char **argv)
+{
+	if (argv[1] && ft_strcmp(argv[1], "--bench") == 0)
+		flags->has_bench = true;
+	if (argv[2] && ft_strcmp(argv[2], "--bench") == 0)
+	{
+		if (flags->has_bench == true)
+			give_error();
+		flags->has_bench = true;
+	}
+}
+
+//--------[returns true if arg is (--bench), false otherwise]-------------//
+
+bool	is_bench_flags(char *flag)
+{
+	if (!ft_strcmp(flag, "--bench"))
+		return (true);
+	return (false);
+}
+
+//----------[checks the disorder of the stack]----------------------------//
+
+float	disorder(t_node *stack)
+{
+	t_node	*node1;
+	t_node	*node2;
+	int		count;
+	int		total_pairs;
+
+	if (ft_lstsize(stack) <= 1)
+		return (0);
+	node1 = stack;
+	count = 0;
+	total_pairs = 0;
+	while (node1)
+	{
+		node2 = node1->next;
+		while (node1 != node2 && node2)
+		{
+			total_pairs++;
+			if (node1->content > node2->content)
+				count++;
+			node2 = node2->next;
+		}
+		node1 = node1->next;
+	}
+	return ((float) count / (float) total_pairs);
+}
+
+//----------[defines difficulty based on disorder level]--------------------//
+
+void	ft_resolve_strategy(t_flags *flag, float disorder)
+{
+	flag->chosen = flag->difficulty;
+	if (disorder < 0.2)
+		flag->difficulty = DIFF_SIMPLE;
+	else if (disorder < 0.5)
+		flag->difficulty = DIFF_MEDIUM;
+	else
+		flag->difficulty = DIFF_COMPLEX;
+}
+
